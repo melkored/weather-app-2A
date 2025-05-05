bcea01cf38a05251ffcb7e91e4ed98e0622b25ee
Eduardo Hernandez <developmelk@gmail.com>
2025-05-05 09:41:37 -0600

    Added Geolocation
    Added Geolocation and success and error logs (it will change in the future to show error messages)

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hola");
+let nav;
+
+function success(position) {
+  console.log("Hola", position);
+  navigator.geolocation.clearWatch(nav);
+}
+
+function error(e) {
+  console.log("ERROR", e);
+}
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+nav = navigator.geolocation.watchPosition(success, error, options);
