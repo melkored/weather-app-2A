c8358e6433953bba855153b84e126f6847b532f3
Eduardo Hernandez <developmelk@gmail.com>
2025-05-07 09:22:21 -0600

    Added javascript request
    Added geolocation request

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 let nav;
+const appID = "06d12b314bb4c17941caf958f729dcdd";
+const weatherBaseApi = "http://api.openweathermap.org/data/2.5/weather";
+const geocodingBaseApi = "http://api.openweathermap.org/geo/1.0/direct";
 
-function success(position) {
-  console.log("Hola", position);
-  navigator.geolocation.clearWatch(nav);
+async function getWeatherConditions(lat, lng) {
+  try {
+    const response = await fetch(
+      `${weatherBaseApi}?lat=${lat}&lon=${lng}&lang=es&units=metric&appid=${appID}`
+    );
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("DATA", data);
+    return data;
+  } catch (error) {
+    console.log("ERROR", error);
+    return error;
+  }
+}
+
+async function success(position) {
+  const { longitude, latitude } = position.coords;
+  try {
+    await getWeatherConditions(latitude, longitude);
+  } catch (error) {
+    console.log("ERROR", error);
+  } finally {
+    navigator.geolocation.clearWatch(nav);
+  }
 }
 
 function error(e) {
@@ -15,4 +41,8 @@ options = {
   maximumAge: 0,
 };
 
-nav = navigator.geolocation.watchPosition(success, error, options);
+if (navigator.geolocation) {
+  nav = navigator.geolocation.watchPosition(success, error, options);
+} else {
+  console.error("Geolocalización no soportada en este navegador");
+}
